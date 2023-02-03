283e2d4abb74c40be7b9a5bd9b0f62ac6036f59a
Cody Fletcher <fletccod@sheridancollege.ca>
2023-02-03 11:25:50 -0500

    Added code for randomized card, array for hand and user input

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,7 @@
 package exercise1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -12,17 +14,37 @@ public class CardTrick {
     
     public static void main(String[] args) {
         
+        Scanner kb = new Scanner (System.in);
+        
         Card[] hand = new Card[7];
+        int max = 52;
+        int min = 1;
 
+        //Creates 7 random cards into an array
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            card.setValue((int) (Math.random() * ((max - min) + min) * 100));
+            card.setSuit(RandomSuit assigned);
+            
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
-
+        System.out.println("Enter a suit, then the number for the card: ");
+        String suit = kb.next();
+        int cardNum = kb.nextInt();
+        
+        for (int i=0; i<hand.length; i++){
+            if (suit == hand[i].setSuit && cardNum == hand[i].setValue){
+                System.out.println("card found");
+            }
+            else {
+                System.out.println("card not found");
+            }
+           
+        }
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
